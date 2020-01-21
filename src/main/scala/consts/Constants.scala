34ac5f61276eed0ee5d5a602262a2d879230a37d
MaxXSoft <x@MaxXSoft.net>
2020-01-21 19:44:08 +0800

    (Constants) added more constants

--- src/main/scala/consts/Constants.scala
+++ src/main/scala/consts/Constants.scala
@@ -1,8 +1,15 @@
 package consts
 
 import chisel3._
+import chisel3.util.log2Ceil
 
 object Constants {
-  val ADDR_WIDTH = 32
-  val DATA_WIDTH = 32
+  val ADDR_WIDTH      = 32
+  val DATA_WIDTH      = 32
+  val INST_WIDTH      = 32
+
+  val REG_COUNT       = 32
+  val REG_ADDR_WIDTH  = log2Ceil(REG_COUNT)
+
+  val RESET_PC        = "h00000200".U(ADDR_WIDTH.W)
 }
