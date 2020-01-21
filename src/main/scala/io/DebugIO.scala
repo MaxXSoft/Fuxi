e34e926a8baa4fbf356424108a2a0e719d864fc2
MaxXSoft <x@MaxXSoft.net>
2020-01-21 19:44:25 +0800

    (DebugIO) added bundle 'DebugIO' for debugging

---
+++ src/main/scala/io/DebugIO.scala
@@ -0,0 +1,12 @@
+package io
+
+import chisel3._
+
+import consts.Constants._
+
+class DebugIO extends Bundle {
+  val regWen    = Output(Bool())
+  val regWaddr  = Output(UInt(REG_ADDR_WIDTH.W))
+  val regWdata  = Output(UInt(DATA_WIDTH.W))
+  val pc        = Output(UInt(ADDR_WIDTH.W))
+}
