1d1e96dce4ff2979b8b90acc65bc309dbec72f24
MaxXSoft <x@MaxXSoft.net>
2020-01-21 19:21:32 +0800

    (SramIO) added interface for SRAM peripherals

---
+++ src/main/scala/io/SramIO.scala
@@ -0,0 +1,12 @@
+package io
+
+import chisel3._
+
+class SramIO(addrWidth: Int, dataWidth: Int) extends Bundle {
+  val en    = Output(Bool())
+  val valid = Input(Bool())
+  val wen   = Output(UInt((dataWidth / 8).W))
+  val addr  = Output(UInt(addrWidth.W))
+  val rdata = Input(UInt(dataWidth.W))
+  val wdata = Output(UInt(dataWidth.W))
+}
