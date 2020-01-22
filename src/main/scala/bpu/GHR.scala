f1c8823c3bd302a753400391e50313a4672bc444
MaxXSoft <x@MaxXSoft.net>
2020-01-22 16:48:00 +0800

    (GHR) added module 'GHR'

---
+++ src/main/scala/bpu/GHR.scala
@@ -0,0 +1,25 @@
+package bpu
+
+import chisel3._
+import chisel3.util.Cat
+
+import consts.Constants.GHR_WIDTH
+
+// global history register
+class GHR extends Module {
+  val io = IO(new Bundle {
+    // branch information
+    val branch  = Input(Bool())
+    val taken   = Input(Bool())
+    // value of GHR
+    val ghr     = Output(UInt(GHR_WIDTH.W))
+  })
+
+  val ghr = Reg(UInt(GHR_WIDTH.W))
+
+  when (io.branch) {
+    ghr := Cat(ghr(GHR_WIDTH - 2, 0), io.taken)
+  }
+
+  io.ghr := ghr
+}
