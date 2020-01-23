951a02bebe3b62cf6f40be32481cbc94d585a027
MaxXSoft <x@MaxXSoft.net>
2020-01-23 16:28:04 +0800

    (BranchInfoIO) added IO 'BranchInfoIO'

---
+++ src/main/scala/io/BranchInfoIO.scala
@@ -0,0 +1,14 @@
+package io
+
+import chisel3._
+
+import consts.Constants.{GHR_WIDTH, ADDR_WIDTH}
+
+class BranchInfoIO extends Bundle {
+  val branch  = Bool()              // last inst is a b/j
+  val jump    = Bool()              // is 'jal' or 'jalr'
+  val taken   = Bool()              // is last branch taken
+  val index   = UInt(GHR_WIDTH.W)   // last index of PHT
+  val pc      = UInt(ADDR_WIDTH.W)  // last instruction PC
+  val target  = UInt(ADDR_WIDTH.W)  // last branch target
+}
