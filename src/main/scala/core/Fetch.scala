9ab7d2abc845d4526f42013dd7e2d7c7012c00ba
MaxXSoft <x@MaxXSoft.net>
2020-01-23 17:46:28 +0800

    (Fetch) added instruction fetch stage

---
+++ src/main/scala/core/Fetch.scala
@@ -0,0 +1,47 @@
+package core
+
+import chisel3._
+
+import io._
+import consts.Constants._
+import bpu.BranchPredictor
+
+class Fetch extends Module {
+  val io = IO(new Bundle {
+    // pipeline control signals
+    val flush   = Input(Bool())
+    val stall   = Input(Bool())
+    val excPc   = Input(UInt(ADDR_WIDTH.W))
+    // ROM interface
+    val rom     = new SramIO(ADDR_WIDTH, DATA_WIDTH)
+    // branch information (from decoder)
+    val branch  = Input(new BranchInfoIO)
+    // to next stage
+    val fetch   = Output(new FetchIO)
+  })
+
+  // program counter
+  val pc = RegInit(0.U(ADDR_WIDTH.W))
+
+  // instantiate branch predictor
+  val bpu = Module(new BranchPredictor)
+  bpu.io.branchInfo <> io.branch
+  bpu.io.lookupPc   := pc
+
+  // update PC
+  val nextPc = Mux(io.flush, io.excPc,
+               Mux(io.stall || !io.rom.valid, pc,
+               Mux(bpu.io.predTaken, bpu.io.predTarget,
+                   pc + (INST_WIDTH / 8).U)))
+  pc := nextPc
+
+  // generate ROM signals
+  io.rom.en     := true.B
+  io.rom.wen    := 0.U
+  io.rom.addr   := nextPc
+  io.rom.wdata  := 0.U
+
+  // generate output
+  io.fetch.inst := io.rom.rdata
+  io.fetch.pc   := pc
+}
