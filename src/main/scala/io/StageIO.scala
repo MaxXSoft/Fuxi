0ec764b12328651d9bb293e75e4d7be12a453997
MaxXSoft <x@MaxXSoft.net>
2020-01-23 17:46:45 +0800

    (StageIO) added IO 'FetchIO'

--- src/main/scala/io/StageIO.scala
+++ src/main/scala/io/StageIO.scala
@@ -2,8 +2,26 @@ package io
 
 import chisel3._
 
+import consts.Constants._
+import consts.Instructions
+
 // interface of stage's IO
 class StageIO[T <: StageIO[T]] extends Bundle {
   this: T =>
+
+  // for initializing flip-flops in mid-stage
   def default() = 0.U.asTypeOf(this)
+}
+
+// IF stage
+class FetchIO extends StageIO[FetchIO] {
+  val inst  = UInt(INST_WIDTH.W)
+  val pc    = UInt(ADDR_WIDTH.W)
+
+  override def default() = {
+    val init = Wire(new FetchIO)
+    init.inst := Instructions.NOP
+    init.pc := 0.U
+    init
+  }
 }
