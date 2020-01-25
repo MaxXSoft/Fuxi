a70b39ff8c2e8d6bbeca96a4a4de47a0afdf1014
MaxXSoft <x@MaxXSoft.net>
2020-01-25 09:05:41 +0800

    (RegFile) added module 'RegFile'

---
+++ src/main/scala/storage/RegFile.scala
@@ -0,0 +1,26 @@
+package storage
+
+import chisel3._
+
+import io._
+import consts.Constants._
+
+class RegFile extends Module {
+  val io = IO(new Bundle {
+    val read1 = Flipped(new RegReadIO)
+    val read2 = Flipped(new RegReadIO)
+    val write = Flipped(new RegWriteIO)
+  })
+
+  val init = Seq.fill(REG_COUNT) { 0.U(DATA_WIDTH.W) }
+  val regfile = RegInit(VecInit(init))
+
+  // read channels
+  io.read1.data := Mux(io.read1.en, regfile(io.read1.addr), 0.U)
+  io.read2.data := Mux(io.read2.en, regfile(io.read2.addr), 0.U)
+
+  // write channel
+  when (io.write.en && io.write.addr =/= 0.U) {
+    regfile(io.write.addr) := io.write.data
+  }
+}
