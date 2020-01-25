b906ddd7be5dc374d7e2555b2f6e50d6155d02db
MaxXSoft <x@MaxXSoft.net>
2020-01-25 18:35:08 +0800

    (SramIO) added a signal to indicate page fault

--- src/main/scala/io/SramIO.scala
+++ src/main/scala/io/SramIO.scala
@@ -5,6 +5,7 @@ import chisel3._
 class SramIO(val addrWidth: Int, val dataWidth: Int) extends Bundle {
   val en    = Output(Bool())
   val valid = Input(Bool())
+  val fault = Input(Bool())
   val wen   = Output(UInt((dataWidth / 8).W))
   val addr  = Output(UInt(addrWidth.W))
   val rdata = Input(UInt(dataWidth.W))
