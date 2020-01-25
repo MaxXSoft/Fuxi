281796c468a7f944ac18e70e76e6f7d756703fa6
MaxXSoft <x@MaxXSoft.net>
2020-01-25 19:17:48 +0800

    (StageIO) finished module 'DecoderIO'

--- src/main/scala/io/StageIO.scala
+++ src/main/scala/io/StageIO.scala
@@ -3,7 +3,6 @@ package io
 import chisel3._
 
 import consts.Constants._
-import consts.Instructions
 
 // interface of stage's IO
 class StageIO[T <: StageIO[T]] extends Bundle {
@@ -15,20 +14,46 @@ class StageIO[T <: StageIO[T]] extends Bundle {
 
 // IF stage
 class FetchIO extends StageIO[FetchIO] {
+  import consts.Instructions.NOP
+
   val inst      = UInt(INST_WIDTH.W)
   val pc        = UInt(ADDR_WIDTH.W)
   val predIndex = UInt(ADDR_WIDTH.W)
+  val pageFault = Bool()
 
   override def default() = {
     val init = Wire(new FetchIO)
-    init.inst := Instructions.NOP
+    init.inst := NOP
     init.pc := 0.U
     init.predIndex := 0.U
+    init.pageFault := false.B
     init
   }
 }
 
 // ID stage
 class DecoderIO extends StageIO[DecoderIO] {
-  //
+  import consts.AluOp.ALU_OP_WIDTH
+  import consts.MduOp.MDU_OP_WIDTH
+  import consts.LsuOp.LSU_OP_WIDTH
+  import consts.CsrOp.CSR_OP_WIDTH
+  import consts.ExceptType.EXC_TYPE_WIDTH
+
+  // to ALU/MDU
+  val aluOp     = UInt(ALU_OP_WIDTH.W)
+  val opr1      = SInt(DATA_WIDTH.W)
+  val opr2      = SInt(DATA_WIDTH.W)
+  val mduOp     = UInt(MDU_OP_WIDTH.W)
+  // to Mem (LSU)
+  val lsuOp     = UInt(LSU_OP_WIDTH.W)
+  val lsuData   = UInt(DATA_WIDTH.W)
+  // to write back
+  val regWen    = Bool()
+  val regWaddr  = UInt(REG_ADDR_WIDTH.W)
+  // to CSR
+  val csrOp     = UInt(CSR_OP_WIDTH.W)
+  val csrAddr   = UInt(CSR_ADDR_WIDTH.W)
+  val csrData   = UInt(DATA_WIDTH.W)
+  // exception type
+  val excType   = UInt(EXC_TYPE_WIDTH.W)
 }
