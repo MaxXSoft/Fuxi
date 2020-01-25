849ab7901a755982c10ca2c6c124074df9701a70
MaxXSoft <x@MaxXSoft.net>
2020-01-25 09:05:18 +0800

    (RegIO) added IO interfaces for register file

---
+++ src/main/scala/io/RegIO.scala
@@ -0,0 +1,17 @@
+package io
+
+import chisel3._
+
+import consts.Constants._
+
+class RegReadIO extends Bundle {
+  val en    = Output(Bool())
+  val addr  = Output(UInt(REG_ADDR_WIDTH.W))
+  val data  = Input(UInt(DATA_WIDTH.W))
+}
+
+class RegWriteIO extends Bundle {
+  val en    = Output(Bool())
+  val addr  = Output(UInt(REG_ADDR_WIDTH.W))
+  val data  = Output(UInt(DATA_WIDTH.W))
+}
