9ab4a5ff58c30cb892e5ac272a033cd08f0e4189
MaxXSoft <x@MaxXSoft.net>
2020-01-26 20:54:12 +0800

    (DividerTest) added testbench for module 'Divider'

---
+++ src/test/scala/mdu/DividerTest.scala
@@ -0,0 +1,33 @@
+package mdu
+
+import chisel3.iotesters.{Driver, PeekPokeTester}
+
+class DividerUnitTester(c: Divider) extends PeekPokeTester(c) {
+  def testDivider() = {
+    // generate operands and result
+    val divident  = BigInt(rnd.nextInt.toHexString, 16)
+    val divisor   = BigInt(rnd.nextInt.toHexString, 16)
+    // println(s"divident: $divident, divisor: $divisor")
+    val quotient  = divident / divisor
+    val remainder = divident % divisor
+    // poke & expect
+    poke(c.io.en, true)
+    poke(c.io.divident, divident)
+    poke(c.io.divisor, divisor)
+    do {
+      step(1)
+    } while (peek(c.io.done).toInt == 0)
+    expect(c.io.quotient, quotient)
+    expect(c.io.remainder, remainder)
+  }
+
+  for (i <- 0 until 20) {
+    testDivider()
+  }
+}
+
+object DividerTest extends App {
+  Driver.execute(args, () => new Divider(32)) {
+    (c) => new DividerUnitTester(c)
+  }
+}
