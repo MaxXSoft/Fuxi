d83a703cf196a355c637fc26a3f23c8ce51fac18
MaxXSoft <x@MaxXSoft.net>
2020-01-27 11:08:31 +0800

    (Multiplier) added flush signal

--- src/main/scala/mdu/Multiplier.scala
+++ src/main/scala/mdu/Multiplier.scala
@@ -3,11 +3,12 @@ package mdu
 import chisel3._
 import chisel3.util._
 
-// N-bit multiplier
+// N-bit pipelined multiplier
 class Multiplier(val oprWidth: Int, val latency: Int = 0) extends Module {
   val io = IO(new Bundle {
     // control signals
     val en    = Input(Bool())
+    val flush = Input(Bool())
     val done  = Output(Bool())
     // operands & results
     val opr1  = Input(UInt(oprWidth.W))
@@ -16,12 +17,26 @@ class Multiplier(val oprWidth: Int, val latency: Int = 0) extends Module {
     val hi    = Output(UInt(oprWidth.W))
   })
 
+  require(latency >= 0,
+          "Multiplier latency must be greater than or equal to zero!")
+
   // product
   val result  = io.opr1 * io.opr2
 
+  def generatePipe(en: Bool, data: UInt, latency: Int): (Bool, UInt) = {
+    if (latency == 0) {
+      (en, data)
+    }
+    else {
+      val done  = RegNext(Mux(io.flush, false.B, en), false.B)
+      val bits  = RegEnable(data, en)
+      generatePipe(done, bits, latency - 1)
+    }
+  }
+
   // generate output
-  val piped = Pipe(io.en, result, latency)
-  io.done  := piped.valid
-  io.lo    := piped.bits(oprWidth - 1, 0)
-  io.hi    := piped.bits(oprWidth * 2 - 1, oprWidth)
+  val (en, data)  = generatePipe(io.en, result, latency)
+  io.done        := en
+  io.lo          := data(oprWidth - 1, 0)
+  io.hi          := data(oprWidth * 2 - 1, oprWidth)
 }
