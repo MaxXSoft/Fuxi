c9c53c11b5dfcdfa50a57330e2d5646097a52083
MaxXSoft <x@MaxXSoft.net>
2020-01-28 16:05:30 +0800

    (ExclusiveMonitorTest) added a new testbench

---
+++ src/test/scala/lsu/ExclusiveMonitorTest.scala
@@ -0,0 +1,51 @@
+package lsu
+
+import chisel3.iotesters.{Driver, PeekPokeTester}
+
+class ExclusiveMonitorUnitTester(c: ExclusiveMonitor)
+      extends PeekPokeTester(c) {
+  val addr1 = 0x12345678
+  val addr2 = 0x234abfec
+
+  poke(c.io.set, false)
+  poke(c.io.clear, false)
+  poke(c.io.addr, addr1)
+  step(1)
+  expect(c.io.valid, false)
+
+  poke(c.io.set, true)
+  poke(c.io.clear, false)
+  poke(c.io.addr, addr1)
+  step(1)
+  expect(c.io.valid, true)
+
+  poke(c.io.set, false)
+  poke(c.io.clear, false)
+  poke(c.io.addr, addr1)
+  step(1)
+  expect(c.io.valid, true)
+
+  poke(c.io.set, false)
+  poke(c.io.clear, false)
+  poke(c.io.addr, addr2)
+  step(1)
+  expect(c.io.valid, false)
+
+  poke(c.io.set, true)
+  poke(c.io.clear, false)
+  poke(c.io.addr, addr2)
+  step(1)
+  expect(c.io.valid, true)
+
+  poke(c.io.set, false)
+  poke(c.io.clear, true)
+  poke(c.io.addr, addr2)
+  step(1)
+  expect(c.io.valid, false)
+}
+
+object ExclusiveMonitorTest extends App {
+  Driver.execute(args, () => new ExclusiveMonitor) {
+    (c) => new ExclusiveMonitorUnitTester(c)
+  }
+}
