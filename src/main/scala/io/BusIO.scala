de962877bf0928f2314cabc9ca3a9ccf9953c6f6
MaxXSoft <x@MaxXSoft.net>
2020-01-28 09:21:51 +0800

    (BusIO) renamed file 'SramIO.scala'

--- src/main/scala/io/BusIO.scala
+++ src/main/scala/io/BusIO.scala
@@ -2,12 +2,17 @@ package io
 
 import chisel3._
 
+// SRAM interface
 class SramIO(val addrWidth: Int, val dataWidth: Int) extends Bundle {
   val en    = Output(Bool())
-  val valid = Input(Bool())
-  val fault = Input(Bool())
+  val valid = Input(Bool())                     // data valid
+  val fault = Input(Bool())                     // page fault
   val wen   = Output(UInt((dataWidth / 8).W))
   val addr  = Output(UInt(addrWidth.W))
   val rdata = Input(UInt(dataWidth.W))
   val wdata = Output(UInt(dataWidth.W))
+}
+
+class CacheIO extends Bundle {
+  val flush = Bool()
 }
