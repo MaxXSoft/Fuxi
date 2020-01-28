7a44486430d6df6b233856ac17fc09081f5f4f2a
MaxXSoft <x@MaxXSoft.net>
2020-01-28 19:33:43 +0800

    (AmoExecuteTest) added testbench for 'AmoExecute'

---
+++ src/test/scala/lsu/AmoExecuteTest.scala
@@ -0,0 +1,46 @@
+package lsu
+
+import chisel3.iotesters.{Driver, PeekPokeTester}
+
+import LsuDecode._
+
+class AmoExecuteUnitTester(c: AmoExecute) extends PeekPokeTester(c) {
+  def testAmo() = {
+    val opr1 = BigInt(rnd.nextInt.toHexString, 16)
+    val opr2 = BigInt(rnd.nextInt.toHexString, 16)
+    val mask = BigInt("ffffffff", 16)
+
+    def simulateLatency() = {
+      val latency = rnd.nextInt(32)
+      for (i <- 0 until latency) {
+        poke(c.io.ramValid, false)
+        step(1)
+      }
+      poke(c.io.ramValid, true)
+      poke(c.io.ramRdata, opr1)
+      step(1)
+    }
+
+    poke(c.io.op, AMO_OP_ADD)
+    poke(c.io.flush, false)
+    poke(c.io.regOpr, opr2)
+    poke(c.io.ramRdata, 0)
+    simulateLatency()
+    expect(c.io.ready, false)
+    expect(c.io.ramWen, true)
+    expect(c.io.ramWdata, (opr1 + opr2) & mask)
+    simulateLatency()
+    expect(c.io.ready, true)
+    expect(c.io.regWdata, opr1)
+  }
+
+  for (i <- 0 until 20) {
+    testAmo()
+  }
+}
+
+object AmoExecuteTest extends App {
+  Driver.execute(args, () => new AmoExecute) {
+    (c) => new AmoExecuteUnitTester(c)
+  }
+}
