8968adca5cfd9719600f31d486eac527de2ef308
MaxXSoft <x@MaxXSoft.net>
2020-01-29 17:21:54 +0800

    (ExclusiveMonitor) modified some details about IO

--- src/main/scala/lsu/ExclusiveMonitor.scala
+++ src/main/scala/lsu/ExclusiveMonitor.scala
@@ -7,25 +7,26 @@ import consts.Parameters._
 // for LR & SC instructions
 class ExclusiveMonitor extends Module {
   val io = IO(new Bundle {
-    // write channel
-    val set       = Input(Bool())
-    val clear     = Input(Bool())
-    val addrSet   = Input(UInt(ADDR_WIDTH.W))
-    // check channel
-    val addrCheck = Input(UInt(ADDR_WIDTH.W))
-    val valid     = Output(Bool())
+    // pipeline control
+    val flush = Input(Bool())
+    // modify
+    val set   = Input(Bool())
+    val clear = Input(Bool())
+    val addr  = Input(UInt(ADDR_WIDTH.W))
+    // check
+    val valid = Output(Bool())
   })
 
   val flag  = RegInit(false.B)
   val addr  = RegInit(0.U(ADDR_WIDTH.W))
 
-  when (io.clear) {
+  when (io.flush || io.clear) {
     flag  := false.B
     addr  := 0.U
   } .elsewhen (io.set) {
     flag  := true.B
-    addr  := io.addrSet
+    addr  := io.addr
   }
 
-  io.valid := flag && addr === io.addrCheck
+  io.valid := flag && addr === io.addr
 }
