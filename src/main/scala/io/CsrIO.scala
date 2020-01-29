89860f17c7504550a8b467efc674004e11563c7c
MaxXSoft <x@MaxXSoft.net>
2020-01-29 17:54:37 +0800

    (CsrIO) added some IO interfaces about CSR

---
+++ src/main/scala/io/CsrIO.scala
@@ -0,0 +1,26 @@
+package io
+
+import chisel3._
+
+import consts.Parameters.DATA_WIDTH
+import consts.CSR.CSR_ADDR_WIDTH
+import consts.CsrOp.CSR_OP_WIDTH
+
+class CsrReadIO extends Bundle {
+  val en    = Output(Bool())
+  val valid = Input(Bool())
+  val addr  = Output(UInt(CSR_ADDR_WIDTH.W))
+  val data  = Input(UInt(DATA_WIDTH.W))
+}
+
+class CsrWriteIO extends Bundle {
+  val op    = Output(UInt(CSR_OP_WIDTH.W))
+  val addr  = Output(UInt(CSR_ADDR_WIDTH.W))
+  val data  = Output(UInt(DATA_WIDTH.W))
+}
+
+class CsrCommitIO extends Bundle {
+  val op    = UInt(CSR_OP_WIDTH.W)
+  val addr  = UInt(CSR_ADDR_WIDTH.W)
+  val data  = UInt(DATA_WIDTH.W)
+}
