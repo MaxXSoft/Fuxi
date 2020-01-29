18a6ecf889e0d2cb72227185ab94c2cc6cfde113
MaxXSoft <x@MaxXSoft.net>
2020-01-29 09:09:30 +0800

    (ExceptInfoIO) added IO interface 'ExceptInfoIO'

---
+++ src/main/scala/io/ExceptInfoIO.scala
@@ -0,0 +1,15 @@
+package io
+
+import chisel3._
+
+import consts.Parameters._
+import consts.ExceptCause.EXC_CAUSE_WIDTH
+
+class ExceptInfoIO extends Bundle {
+  val hasExcept = Bool()
+  val isSret    = Bool()
+  val isMret    = Bool()
+  val excCause  = UInt(EXC_CAUSE_WIDTH.W)
+  val excPc     = UInt(ADDR_WIDTH.W)
+  val excValue  = UInt(DATA_WIDTH.W)
+}
