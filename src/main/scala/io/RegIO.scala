84e4ffc3ae61da0bf2542fcc58fcc5b5031847ff
MaxXSoft <x@MaxXSoft.net>
2020-01-29 09:22:51 +0800

    (RegIO) added IO interface 'RegCommitIO'

--- src/main/scala/io/RegIO.scala
+++ src/main/scala/io/RegIO.scala
@@ -15,3 +15,11 @@ class RegWriteIO extends Bundle {
   val addr  = Output(UInt(REG_ADDR_WIDTH.W))
   val data  = Output(UInt(DATA_WIDTH.W))
 }
+
+class RegCommitIO extends Bundle {
+  val en    = Bool()
+  val addr  = UInt(REG_ADDR_WIDTH.W)
+  val data  = UInt(DATA_WIDTH.W)
+  // detect load hazard
+  val load  = Bool()
+}
