34aadd74eb06c8bf187fe9082e3e30e20ba6cd5c
MaxXSoft <x@MaxXSoft.net>
2020-01-30 17:28:49 +0800

    (ExclusiveMonitor) modified some details

--- src/main/scala/lsu/ExclusiveMonitor.scala
+++ src/main/scala/lsu/ExclusiveMonitor.scala
@@ -2,31 +2,30 @@ package lsu
 
 import chisel3._
 
+import io._
 import consts.Parameters._
 
 // for LR & SC instructions
 class ExclusiveMonitor extends Module {
   val io = IO(new Bundle {
     // pipeline control
-    val flush = Input(Bool())
-    // modify
-    val set   = Input(Bool())
-    val clear = Input(Bool())
-    val addr  = Input(UInt(ADDR_WIDTH.W))
-    // check
-    val valid = Output(Bool())
+    val flush   = Input(Bool())
+    // check channel
+    val check   = Flipped(new ExcMonCheckIO)
+    // update channel
+    val update  = Flipped(new ExcMonUpdateIO)
   })
 
   val flag  = RegInit(false.B)
   val addr  = RegInit(0.U(ADDR_WIDTH.W))
 
-  when (io.flush || io.clear) {
+  when (io.flush || io.update.clear) {
     flag  := false.B
     addr  := 0.U
-  } .elsewhen (io.set) {
+  } .elsewhen (io.update.set) {
     flag  := true.B
-    addr  := io.addr
+    addr  := io.update.addr
   }
 
-  io.valid := flag && addr === io.addr
+  io.check.valid := flag && addr === io.check.addr
 }
