86f74eb952e43c1ba941aa47ebd2d3212bb04182
MaxXSoft <x@MaxXSoft.net>
2020-02-03 13:19:00 +0800

    (BusIO) added some new IO interfaces

--- src/main/scala/io/BusIO.scala
+++ src/main/scala/io/BusIO.scala
@@ -4,15 +4,39 @@ import chisel3._
 
 // SRAM interface
 class SramIO(val addrWidth: Int, val dataWidth: Int) extends Bundle {
+  // enable signal
   val en    = Output(Bool())
-  val valid = Input(Bool())                     // data valid
-  val fault = Input(Bool())                     // page fault
+  // data valid flag
+  val valid = Input(Bool())
+  // page fault flag
+  val fault = Input(Bool())
+  // other signals
   val wen   = Output(UInt((dataWidth / 8).W))
   val addr  = Output(UInt(addrWidth.W))
   val rdata = Input(UInt(dataWidth.W))
   val wdata = Output(UInt(dataWidth.W))
 }
 
-class CacheIO extends Bundle {
-  val flush = Bool()
+// TLB control
+class TlbControlIO(val addrWidth: Int) extends Bundle {
+  // enable address translation
+  val en        = Output(Bool())
+  // flush signals
+  val flushInst = Output(Bool())
+  val flushData = Output(Bool())
+  // base address of page table
+  val base      = Output(UInt(addrWidth.W))
+}
+
+// cache control
+class CacheControlIO extends Bundle {
+  val flushInst = Output(Bool())
+  val flushData = Output(Bool())
+}
+
+// interrupt request
+class InterruptIO extends Bundle {
+  val timer   = Input(Bool())
+  val soft    = Input(Bool())
+  val extern  = Input(Bool())
 }
