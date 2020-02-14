29ff94b38d3aa44c3546ce29d4535685b53b87b3
MaxXSoft <x@MaxXSoft.net>
2020-02-14 16:48:02 +0800

    (SramDemux2) added module 'SramDemux2'

--- src/main/scala/bus/SramUtils.scala
+++ src/main/scala/bus/SramUtils.scala
@@ -26,3 +26,25 @@ class SramMux2(val addrWidth: Int, val dataWidth: Int) extends Module {
   io.out2.addr  := io.in.addr
   io.out2.wdata := io.in.wdata
 }
+
+class SramDemux2(val addrWidth: Int, val dataWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val sel2  = Input(Bool())
+    val in1   = Flipped(new SramIO(addrWidth, dataWidth))
+    val in2   = Flipped(new SramIO(addrWidth, dataWidth))
+    val out   = new SramIO(addrWidth, dataWidth)
+  })
+
+  io.in1.valid  := !io.sel2 && io.out.valid
+  io.in1.fault  := !io.sel2 && io.out.fault
+  io.in1.rdata  := io.out.rdata
+
+  io.in2.valid  := io.sel2 && io.out.valid
+  io.in2.fault  := io.sel2 && io.out.fault
+  io.in2.rdata  := io.out.rdata
+
+  io.out.en     := Mux(io.sel2, io.in2.en, io.in1.en)
+  io.out.wen    := Mux(io.sel2, io.in2.wen, io.in1.wen)
+  io.out.addr   := Mux(io.sel2, io.in2.addr, io.in1.addr)
+  io.out.wdata  := Mux(io.sel2, io.in2.wdata, io.in1.wdata)
+}
