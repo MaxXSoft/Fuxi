c0a1f2cc78a499be619b940e89108a80eeda9c9b
MaxXSoft <x@MaxXSoft.net>
2020-02-17 17:45:41 +0800

    (SramMux2) fixed some bugs about enable signals

--- src/main/scala/bus/SramUtils.scala
+++ src/main/scala/bus/SramUtils.scala
@@ -16,12 +16,12 @@ class SramMux2(val addrWidth: Int, val dataWidth: Int) extends Module {
   io.in.fault := Mux(io.sel2, io.out2.fault, io.out1.fault)
   io.in.rdata := Mux(io.sel2, io.out2.rdata, io.out1.rdata)
 
-  io.out1.en    := !io.sel2
+  io.out1.en    := io.in.en && !io.sel2
   io.out1.wen   := io.in.wen
   io.out1.addr  := io.in.addr
   io.out1.wdata := io.in.wdata
 
-  io.out2.en    := io.sel2
+  io.out2.en    := io.in.en && io.sel2
   io.out2.wen   := io.in.wen
   io.out2.addr  := io.in.addr
   io.out2.wdata := io.in.wdata
