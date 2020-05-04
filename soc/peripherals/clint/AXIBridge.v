b830ebd20e09426f6c46e6971d8cad9717c4b774
MaxXSoft <x@MaxXSoft.net>
2020-05-04 11:36:34 +0800

    (soc/peripherals/clint) added peripheral 'clint'

---
+++ soc/peripherals/clint/AXIBridge.v
@@ -0,0 +1,137 @@
+`timescale 1ns / 1ps
+
+module AXIBridge(
+  input           clk,
+  input           rst_n,
+  // AXI slave interface
+  input   [3:0]   axi_arid,
+  input   [31:0]  axi_araddr,
+  input   [7:0]   axi_arlen,
+  input   [2:0]   axi_arsize,
+  input   [1:0]   axi_arburst,
+  input   [1:0]   axi_arlock,
+  input   [3:0]   axi_arcache,
+  input   [2:0]   axi_arprot,
+  input           axi_arvalid,
+  output          axi_arready,
+  output  [3:0]   axi_rid,
+  output  [31:0]  axi_rdata,
+  output  [1:0]   axi_rresp,
+  output          axi_rlast,
+  output          axi_rvalid,
+  input           axi_rready,
+  input   [3:0]   axi_awid,
+  input   [31:0]  axi_awaddr,
+  input   [7:0]   axi_awlen,
+  input   [2:0]   axi_awsize,
+  input   [1:0]   axi_awburst,
+  input   [1:0]   axi_awlock,
+  input   [3:0]   axi_awcache,
+  input   [2:0]   axi_awprot,
+  input           axi_awvalid,
+  output          axi_awready,
+  input   [3:0]   axi_wid,
+  input   [31:0]  axi_wdata,
+  input   [3:0]   axi_wstrb,
+  input           axi_wlast,
+  input           axi_wvalid,
+  output          axi_wready,
+  output  [3:0]   axi_bid,
+  output  [1:0]   axi_bresp,
+  output          axi_bvalid,
+  input           axi_bready,
+  // general peripheral interface
+  output          gpi_read,
+  output          gpi_write,
+  output  [31:0]  gpi_addr,
+  output  [31:0]  gpi_wdata,
+  input   [31:0]  gpi_rdata
+);
+
+  reg busy, write, r_or_w;
+
+  wire    ar_enter    = axi_arvalid & axi_arready;
+  wire    r_retire    = axi_rvalid  & axi_rready & axi_rlast;
+  wire    aw_enter    = axi_awvalid & axi_awready;
+  wire    w_enter     = axi_wvalid  & axi_wready & axi_wlast;
+  wire    b_retire    = axi_bvalid  & axi_bready;
+
+  assign  axi_arready = ~busy & (!r_or_w | !axi_awvalid);
+  assign  axi_awready = ~busy & ( r_or_w | !axi_arvalid);
+
+  reg [3 :0]  buf_id;
+  reg [31:0]  buf_addr;
+  reg [7 :0]  buf_len;
+  reg [2 :0]  buf_size;
+
+  always @(posedge clk) begin
+    if      (!rst_n             ) busy  <= 1'b0;
+    else if (ar_enter | aw_enter) busy  <= 1'b1;
+    else if (r_retire | b_retire) busy  <= 1'b0;
+  end
+
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      r_or_w    <= 1'b0;
+      buf_id    <= 4'b0;
+      buf_addr  <= 32'h0;
+      buf_len   <= 8'b0;
+      buf_size  <= 3'b0;
+    end
+    else if (ar_enter | aw_enter) begin
+      r_or_w    <= ar_enter;
+      buf_id    <= ar_enter ? axi_arid   : axi_awid  ;
+      buf_addr  <= ar_enter ? axi_araddr : axi_awaddr;
+      buf_len   <= ar_enter ? axi_arlen  : axi_awlen ;
+      buf_size  <= ar_enter ? axi_arsize : axi_awsize;
+    end
+  end
+
+  reg wready_reg;
+  assign  axi_wready  = wready_reg;
+  always @(posedge clk) begin
+    if      (!rst_n             ) wready_reg  <= 1'b0;
+    else if (aw_enter           ) wready_reg  <= 1'b1;
+    else if (w_enter & axi_wlast) wready_reg  <= 1'b0;
+  end
+
+  reg rvalid_reg;
+  reg rlast_reg;
+  assign  axi_rvalid  = rvalid_reg;
+  assign  axi_rlast   = rlast_reg;
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      rvalid_reg  <= 1'b0;
+      rlast_reg   <= 1'b0;
+    end
+    else if (busy & r_or_w & !r_retire) begin
+      rvalid_reg  <= 1'b1;
+      rlast_reg   <= 1'b1;
+    end
+    else if (r_retire) begin
+      rvalid_reg  <= 1'b0;
+    end
+  end
+
+  assign  axi_rdata   = gpi_rdata;
+
+  reg bvalid_reg;
+  assign  axi_bvalid  = bvalid_reg;
+  always @(posedge clk) begin
+    if      (!rst_n   ) bvalid_reg  <= 1'b0;
+    else if (w_enter  ) bvalid_reg  <= 1'b1;
+    else if (b_retire ) bvalid_reg  <= 1'b0;
+  end
+
+  assign  axi_rid     = buf_id;
+  assign  axi_bid     = buf_id;
+  assign  axi_bresp   = 2'b0;
+  assign  axi_rresp   = 2'b0;
+
+  assign  gpi_read    = ar_enter;
+  assign  gpi_write   = aw_enter;
+  assign  gpi_addr    = gpi_read ? axi_araddr :
+                        gpi_write ? axi_awaddr : 32'h0;
+  assign  gpi_wdata   = axi_wdata;
+
+endmodule
---
+++ soc/peripherals/clint/Clint.v
@@ -0,0 +1,290 @@
+`timescale 1ns / 1ps
+
+/*
+ * CLINT for Fuxi core
+ *
+ *  address:
+ *    0x000: msip
+ *    0x100: mtimecmp lo
+ *    0x104: mtimecmp hi
+ *    0x108: mtime lo
+ *    0x10c: mtime hi
+ */
+module Clint(
+  input           clk,
+  input           clk_rtc,
+  input           rst_n,
+  // AXI slave interface
+  input   [3:0]   axi_arid,
+  input   [31:0]  axi_araddr,
+  input   [7:0]   axi_arlen,
+  input   [2:0]   axi_arsize,
+  input   [1:0]   axi_arburst,
+  input   [1:0]   axi_arlock,
+  input   [3:0]   axi_arcache,
+  input   [2:0]   axi_arprot,
+  input           axi_arvalid,
+  output          axi_arready,
+  output  [3:0]   axi_rid,
+  output  [31:0]  axi_rdata,
+  output  [1:0]   axi_rresp,
+  output          axi_rlast,
+  output          axi_rvalid,
+  input           axi_rready,
+  input   [3:0]   axi_awid,
+  input   [31:0]  axi_awaddr,
+  input   [7:0]   axi_awlen,
+  input   [2:0]   axi_awsize,
+  input   [1:0]   axi_awburst,
+  input   [1:0]   axi_awlock,
+  input   [3:0]   axi_awcache,
+  input   [2:0]   axi_awprot,
+  input           axi_awvalid,
+  output          axi_awready,
+  input   [3:0]   axi_wid,
+  input   [31:0]  axi_wdata,
+  input   [3:0]   axi_wstrb,
+  input           axi_wlast,
+  input           axi_wvalid,
+  output          axi_wready,
+  output  [3:0]   axi_bid,
+  output  [1:0]   axi_bresp,
+  output          axi_bvalid,
+  input           axi_bready,
+  // interrupt signals
+  output          intr_timer,
+  output          intr_soft
+);
+
+  // address
+  localparam      ADDR_MSIP     = 12'h000;
+  localparam      ADDR_MTIME    = 12'h100,  ADDR_MTIMEH     = 12'h104;
+  localparam      ADDR_MTIMECMP = 12'h108,  ADDR_MTIMECMPH  = 12'h10c;
+
+
+  // convert AXI to peripheral interface
+  wire            gpi_read, gpi_write;
+  wire  [31:0]    gpi_addr, gpi_wdata;
+  wire  [11:0]    gpi_peri_addr;
+  reg   [31:0]    gpi_rdata;
+
+  assign gpi_peri_addr = gpi_addr[11:0];
+
+  AXIBridge axi_bridge(
+    .clk          (clk),
+    .rst_n        (rst_n),
+
+    .axi_arid     (axi_arid),
+    .axi_araddr   (axi_araddr),
+    .axi_arlen    (axi_arlen),
+    .axi_arsize   (axi_arsize),
+    .axi_arburst  (axi_arburst),
+    .axi_arlock   (axi_arlock),
+    .axi_arcache  (axi_arcache),
+    .axi_arprot   (axi_arprot),
+    .axi_arvalid  (axi_arvalid),
+    .axi_arready  (axi_arready),
+    .axi_rid      (axi_rid),
+    .axi_rdata    (axi_rdata),
+    .axi_rresp    (axi_rresp),
+    .axi_rlast    (axi_rlast),
+    .axi_rvalid   (axi_rvalid),
+    .axi_rready   (axi_rready),
+    .axi_awid     (axi_awid),
+    .axi_awaddr   (axi_awaddr),
+    .axi_awlen    (axi_awlen),
+    .axi_awsize   (axi_awsize),
+    .axi_awburst  (axi_awburst),
+    .axi_awlock   (axi_awlock),
+    .axi_awcache  (axi_awcache),
+    .axi_awprot   (axi_awprot),
+    .axi_awvalid  (axi_awvalid),
+    .axi_awready  (axi_awready),
+    .axi_wid      (axi_wid),
+    .axi_wdata    (axi_wdata),
+    .axi_wstrb    (axi_wstrb),
+    .axi_wlast    (axi_wlast),
+    .axi_wvalid   (axi_wvalid),
+    .axi_wready   (axi_wready),
+    .axi_bid      (axi_bid),
+    .axi_bresp    (axi_bresp),
+    .axi_bvalid   (axi_bvalid),
+    .axi_bready   (axi_bready),
+
+    .gpi_read     (gpi_read),
+    .gpi_write    (gpi_write),
+    .gpi_addr     (gpi_addr),
+    .gpi_wdata    (gpi_wdata),
+    .gpi_rdata    (gpi_rdata)
+  );
+
+
+  // registers
+  reg           msip;
+  reg   [31:0]  mtime, mtime_r1, mtime_r2;
+  reg   [31:0]  mtimeh, mtimeh_r1, mtimeh_r2;
+  reg   [31:0]  mtimecmp, mtimecmph;
+  wire  [63:0]  mtime_full, next_mtime_full;
+  wire  [31:0]  next_mtime, next_mtimeh;
+
+  assign mtime_full       = {mtimeh, mtime};
+  assign next_mtime_full  = mtime_full + 64'b1;
+  assign next_mtime       = next_mtime_full[31:0];
+  assign next_mtimeh      = next_mtime_full[63:32];
+
+
+  // interrupt signal output
+  wire  [63:0]  mtime_r2_full, mtimecmp_full;
+
+  assign mtime_r2_full  = {mtimeh_r2, mtime_r2};
+  assign mtimecmp_full  = {mtimecmph, mtimecmp};
+  assign intr_soft      = msip;
+  assign intr_timer     = mtime_r2_full >= mtimecmp_full;
+
+
+  // peripheral read
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      gpi_rdata <= 32'h0;
+    end
+    else if (gpi_read) begin
+      case (gpi_peri_addr)
+        ADDR_MSIP:      gpi_rdata <=  {31'b0, msip};
+        ADDR_MTIME:     gpi_rdata <=  mtime_r2;
+        ADDR_MTIMEH:    gpi_rdata <=  mtimeh_r2;
+        ADDR_MTIMECMP:  gpi_rdata <=  mtimecmp;
+        ADDR_MTIMECMPH: gpi_rdata <=  mtimecmph;
+        default:        gpi_rdata <= 32'h0;
+      endcase
+    end
+  end
+
+
+  // software interrupt
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      msip <= 1'b0;
+    end
+    else if (gpi_write && gpi_peri_addr == ADDR_MSIP) begin
+      msip <= gpi_wdata[0];
+    end
+  end
+
+
+  // mtime register write/update
+  reg         write_mtime_begin, write_mtime_begin_r1;
+  reg         write_mtime_begin_r2, write_mtime_begin_r3;
+  reg         write_mtime_end_r1, write_mtime_end_r2;
+  reg [31:0]  mtime_wdata, mtime_wdata_r1, mtime_wdata_r2;
+  wire        write_mtime = gpi_write && gpi_peri_addr == ADDR_MTIME;
+
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      write_mtime_begin   <= 1'b0;
+    end
+    else if (write_mtime) begin
+      write_mtime_begin   <= 1'b1;
+      mtime_wdata         <= gpi_wdata;
+    end
+    else if (write_mtime_end_r2) begin
+      write_mtime_begin   <= 1'b0;
+    end
+  end
+
+  always @(posedge clk) begin
+    write_mtime_end_r1    <= write_mtime_begin_r2;
+    write_mtime_end_r2    <= write_mtime_end_r1;
+    mtime_r1              <= mtime;
+    mtime_r2              <= mtime_r1;
+  end
+
+  always @(posedge clk_rtc) begin
+    write_mtime_begin_r1  <= write_mtime_begin;
+    write_mtime_begin_r2  <= write_mtime_begin_r1;
+    write_mtime_begin_r3  <= write_mtime_begin_r2;
+    mtime_wdata_r1        <= mtime_wdata;
+    mtime_wdata_r2        <= mtime_wdata_r1;
+  end
+
+  always @(posedge clk_rtc) begin
+    if (!rst_n) begin
+      mtime <= 32'h0;
+    end
+    else if (write_mtime_begin_r2 && !write_mtime_begin_r3) begin
+      mtime <= mtime_wdata_r2;
+    end
+    else begin
+      mtime <= next_mtime;
+    end
+  end
+
+
+  // mtimeh register write/update
+  reg         write_mtimeh_begin, write_mtimeh_begin_r1;
+  reg         write_mtimeh_begin_r2, write_mtimeh_begin_r3;
+  reg         write_mtimeh_end_r1, write_mtimeh_end_r2;
+  reg [31:0]  mtimeh_wdata, mtimeh_wdata_r1, mtimeh_wdata_r2;
+  wire        write_mtimeh = gpi_write && gpi_peri_addr == ADDR_MTIMEH;
+
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      write_mtimeh_begin  <= 1'b0;
+    end
+    else if (write_mtimeh) begin
+      write_mtimeh_begin  <= 1'b1;
+      mtimeh_wdata        <= gpi_wdata;
+    end
+    else if (write_mtimeh_end_r2) begin
+      write_mtimeh_begin  <= 1'b0;
+    end
+  end
+
+  always @(posedge clk) begin
+    write_mtimeh_end_r1   <= write_mtimeh_begin_r2;
+    write_mtimeh_end_r2   <= write_mtimeh_end_r1;
+    mtimeh_r1             <= mtimeh;
+    mtimeh_r2             <= mtimeh_r1;
+  end
+
+  always @(posedge clk_rtc) begin
+    write_mtimeh_begin_r1 <= write_mtimeh_begin;
+    write_mtimeh_begin_r2 <= write_mtimeh_begin_r1;
+    write_mtimeh_begin_r3 <= write_mtimeh_begin_r2;
+    mtimeh_wdata_r1       <= mtimeh_wdata;
+    mtimeh_wdata_r2       <= mtimeh_wdata_r1;
+  end
+
+  always @(posedge clk_rtc) begin
+    if (!rst_n) begin
+      mtimeh <= 32'h0;
+    end
+    else if (write_mtimeh_begin_r2 && !write_mtimeh_begin_r3) begin
+      mtimeh <= mtimeh_wdata_r2;
+    end
+    else begin
+      mtimeh <= next_mtimeh;
+    end
+  end
+
+
+  // mtimecmp register write
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      mtimecmp <= 32'h0;
+    end
+    else if (gpi_write && gpi_peri_addr == ADDR_MTIMECMP) begin
+      mtimecmp <= gpi_wdata;
+    end
+  end
+
+  // mtimecmph register write
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      mtimecmph <= 32'h0;
+    end
+    else if (gpi_write && gpi_peri_addr == ADDR_MTIMECMPH) begin
+      mtimecmph <= gpi_wdata;
+    end
+  end
+
+endmodule
